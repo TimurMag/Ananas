4a58409ef3951219c68384185adef944fc7f94df
TimurMag <srs.magomedov@gmail.com>
2020-07-16 17:52:07 +0300

    aye

--- ananas/ananas/MyForm.h
+++ ananas/ananas/MyForm.h
@@ -35,20 +35,15 @@ namespace ananas {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::Button^ button2;
-	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::RadioButton^ radioButton1;
-	private: System::Windows::Forms::RadioButton^ radioButton2;
-	private: System::Windows::Forms::RadioButton^ radioButton3;
-	private: System::Windows::Forms::RadioButton^ radioButton4;
-	private: System::Windows::Forms::RadioButton^ radioButton5;
-	private: System::Windows::Forms::RadioButton^ radioButton6;
-	private: System::Windows::Forms::RadioButton^ radioButton7;
-	private: System::Windows::Forms::RadioButton^ radioButton8;
-	private: System::Windows::Forms::RadioButton^ radioButton9;
-
 	protected:
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button6;
+	private: System::Windows::Forms::Button^ button7;
+	private: System::Windows::Forms::Button^ button8;
+	private: System::Windows::Forms::Button^ button9;
 
 	private:
 		/// <summary>
@@ -65,193 +60,115 @@ namespace ananas {
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->radioButton1 = (gcnew System::Windows::Forms::RadioButton());
-			this->radioButton2 = (gcnew System::Windows::Forms::RadioButton());
-			this->radioButton3 = (gcnew System::Windows::Forms::RadioButton());
-			this->radioButton4 = (gcnew System::Windows::Forms::RadioButton());
-			this->radioButton5 = (gcnew System::Windows::Forms::RadioButton());
-			this->radioButton6 = (gcnew System::Windows::Forms::RadioButton());
-			this->radioButton7 = (gcnew System::Windows::Forms::RadioButton());
-			this->radioButton8 = (gcnew System::Windows::Forms::RadioButton());
-			this->radioButton9 = (gcnew System::Windows::Forms::RadioButton());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->button8 = (gcnew System::Windows::Forms::Button());
+			this->button9 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(868, 583);
+			this->button1->Location = System::Drawing::Point(23, 22);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(120, 47);
+			this->button1->Size = System::Drawing::Size(147, 59);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"Зашифровать";
+			this->button1->Text = L"RSA";
 			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(721, 583);
+			this->button2->Location = System::Drawing::Point(212, 22);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(120, 47);
+			this->button2->Size = System::Drawing::Size(147, 59);
 			this->button2->TabIndex = 1;
-			this->button2->Text = L"Расшифровать";
+			this->button2->Text = L"Акростих";
 			this->button2->UseVisualStyleBackColor = true;
 			// 
-			// textBox1
+			// button3
 			// 
-			this->textBox1->Location = System::Drawing::Point(175, 32);
-			this->textBox1->Multiline = true;
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(314, 62);
-			this->textBox1->TabIndex = 2;
+			this->button3->Location = System::Drawing::Point(391, 22);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(147, 59);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"Транспозиция";
+			this->button3->UseVisualStyleBackColor = true;
 			// 
-			// label1
+			// button4
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(32, 46);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(86, 31);
-			this->label1->TabIndex = 3;
-			this->label1->Text = L"Текст";
-			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			this->button4->Location = System::Drawing::Point(23, 113);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(147, 59);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"Шифр Виженера";
+			this->button4->UseVisualStyleBackColor = true;
 			// 
-			// radioButton1
+			// button5
 			// 
-			this->radioButton1->AutoSize = true;
-			this->radioButton1->Location = System::Drawing::Point(572, 33);
-			this->radioButton1->Name = L"radioButton1";
-			this->radioButton1->Size = System::Drawing::Size(47, 17);
-			this->radioButton1->TabIndex = 4;
-			this->radioButton1->TabStop = true;
-			this->radioButton1->Text = L"RSA";
-			this->radioButton1->UseVisualStyleBackColor = true;
+			this->button5->Location = System::Drawing::Point(212, 113);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(147, 59);
+			this->button5->TabIndex = 4;
+			this->button5->Text = L"Шифр Цезаря";
+			this->button5->UseVisualStyleBackColor = true;
 			// 
-			// radioButton2
+			// button6
 			// 
-			this->radioButton2->AutoSize = true;
-			this->radioButton2->Location = System::Drawing::Point(572, 54);
-			this->radioButton2->Name = L"radioButton2";
-			this->radioButton2->Size = System::Drawing::Size(108, 17);
-			this->radioButton2->TabIndex = 5;
-			this->radioButton2->TabStop = true;
-			this->radioButton2->Text = L"Шифр Виженера";
-			this->radioButton2->UseVisualStyleBackColor = true;
+			this->button6->Location = System::Drawing::Point(391, 113);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(147, 59);
+			this->button6->TabIndex = 5;
+			this->button6->Text = L"Многоалфавитная замена";
+			this->button6->UseVisualStyleBackColor = true;
 			// 
-			// radioButton3
+			// button7
 			// 
-			this->radioButton3->AutoSize = true;
-			this->radioButton3->Location = System::Drawing::Point(572, 77);
-			this->radioButton3->Name = L"radioButton3";
-			this->radioButton3->Size = System::Drawing::Size(54, 17);
-			this->radioButton3->TabIndex = 6;
-			this->radioButton3->TabStop = true;
-			this->radioButton3->Text = L"ROT1";
-			this->radioButton3->UseVisualStyleBackColor = true;
+			this->button7->Location = System::Drawing::Point(23, 198);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(147, 59);
+			this->button7->TabIndex = 6;
+			this->button7->Text = L"ROT1";
+			this->button7->UseVisualStyleBackColor = true;
 			// 
-			// radioButton4
+			// button8
 			// 
-			this->radioButton4->AutoSize = true;
-			this->radioButton4->Location = System::Drawing::Point(701, 32);
-			this->radioButton4->Name = L"radioButton4";
-			this->radioButton4->Size = System::Drawing::Size(72, 17);
-			this->radioButton4->TabIndex = 7;
-			this->radioButton4->TabStop = true;
-			this->radioButton4->Text = L"Акростих";
-			this->radioButton4->UseVisualStyleBackColor = true;
+			this->button8->Location = System::Drawing::Point(212, 198);
+			this->button8->Name = L"button8";
+			this->button8->Size = System::Drawing::Size(147, 59);
+			this->button8->TabIndex = 7;
+			this->button8->Text = L"Шифр Бейла";
+			this->button8->UseVisualStyleBackColor = true;
 			// 
-			// radioButton5
+			// button9
 			// 
-			this->radioButton5->AutoSize = true;
-			this->radioButton5->Location = System::Drawing::Point(701, 55);
-			this->radioButton5->Name = L"radioButton5";
-			this->radioButton5->Size = System::Drawing::Size(95, 17);
-			this->radioButton5->TabIndex = 8;
-			this->radioButton5->TabStop = true;
-			this->radioButton5->Text = L"Шифр Цезаря";
-			this->radioButton5->UseVisualStyleBackColor = true;
-			this->radioButton5->CheckedChanged += gcnew System::EventHandler(this, &MyForm::radioButton5_CheckedChanged);
-			// 
-			// radioButton6
-			// 
-			this->radioButton6->AutoSize = true;
-			this->radioButton6->Location = System::Drawing::Point(701, 76);
-			this->radioButton6->Name = L"radioButton6";
-			this->radioButton6->Size = System::Drawing::Size(88, 17);
-			this->radioButton6->TabIndex = 9;
-			this->radioButton6->TabStop = true;
-			this->radioButton6->Text = L"Шифр Бейла";
-			this->radioButton6->UseVisualStyleBackColor = true;
-			this->radioButton6->CheckedChanged += gcnew System::EventHandler(this, &MyForm::radioButton6_CheckedChanged);
-			// 
-			// radioButton7
-			// 
-			this->radioButton7->AutoSize = true;
-			this->radioButton7->Location = System::Drawing::Point(834, 33);
-			this->radioButton7->Name = L"radioButton7";
-			this->radioButton7->Size = System::Drawing::Size(98, 17);
-			this->radioButton7->TabIndex = 10;
-			this->radioButton7->TabStop = true;
-			this->radioButton7->Text = L"Транспозиция";
-			this->radioButton7->UseVisualStyleBackColor = true;
-			// 
-			// radioButton8
-			// 
-			this->radioButton8->AutoSize = true;
-			this->radioButton8->Location = System::Drawing::Point(834, 55);
-			this->radioButton8->Name = L"radioButton8";
-			this->radioButton8->Size = System::Drawing::Size(154, 17);
-			this->radioButton8->TabIndex = 11;
-			this->radioButton8->TabStop = true;
-			this->radioButton8->Text = L"Моноалфавитная замена";
-			this->radioButton8->UseVisualStyleBackColor = true;
-			// 
-			// radioButton9
-			// 
-			this->radioButton9->AutoSize = true;
-			this->radioButton9->Location = System::Drawing::Point(834, 77);
-			this->radioButton9->Name = L"radioButton9";
-			this->radioButton9->Size = System::Drawing::Size(96, 17);
-			this->radioButton9->TabIndex = 12;
-			this->radioButton9->TabStop = true;
-			this->radioButton9->Text = L"Азбука морзе";
-			this->radioButton9->UseVisualStyleBackColor = true;
+			this->button9->Location = System::Drawing::Point(391, 198);
+			this->button9->Name = L"button9";
+			this->button9->Size = System::Drawing::Size(147, 59);
+			this->button9->TabIndex = 8;
+			this->button9->Text = L"Азбука морзе";
+			this->button9->UseVisualStyleBackColor = true;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1000, 642);
-			this->Controls->Add(this->radioButton9);
-			this->Controls->Add(this->radioButton8);
-			this->Controls->Add(this->radioButton7);
-			this->Controls->Add(this->radioButton6);
-			this->Controls->Add(this->radioButton5);
-			this->Controls->Add(this->radioButton4);
-			this->Controls->Add(this->radioButton3);
-			this->Controls->Add(this->radioButton2);
-			this->Controls->Add(this->radioButton1);
-			this->Controls->Add(this->label1);
-			this->Controls->Add(this->textBox1);
+			this->ClientSize = System::Drawing::Size(556, 275);
+			this->Controls->Add(this->button9);
+			this->Controls->Add(this->button8);
+			this->Controls->Add(this->button7);
+			this->Controls->Add(this->button6);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-	}
-	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
-	}
-private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
-}
-private: System::Void radioButton5_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
-}
-private: System::Void radioButton6_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
-}
-};
+	};
 }
